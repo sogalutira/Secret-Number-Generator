a465f32444b79d4b8a221da6f13f34f0d70fecdc
sgalutira <sheena.galutira@gmail.com>
2016-05-16 16:34:26 -1000

    numbers are secrets

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,8 @@
 'use strict';
 module.exports = function() {
-  
+  var secretNum = Math.floor(Math.random()*(1000000 - 0 +1)) + 0;
+    return function generatedNum(){
+      return secretNum;
 };
+};
+
